54b406c17fc7952612118b50f47a58e5c7adc95e
Dorian Vinai <dorian.vinai@epita.fr>
2020-09-19 17:36:32 +0200

    coding style: updated .eslintrc.json and fixed whitespace errors

--- filters/blagues.js
+++ filters/blagues.js
@@ -7,20 +7,20 @@ module.exports.regex = [
 
 module.exports.exec = (str, info, funcs) => {
     if (/^re[^a-zA-Z02-9\u00C0-\u017F]*$/i.test(str)) {
-        if (/^RE[^a-zA-Z02-9\u00C0-\u017F]*$/.test(str)) 
+        if (/^RE[^a-zA-Z02-9\u00C0-\u017F]*$/.test(str))
             funcs.answer("QUIN" + str.substring(2) + " 🦈")
-        else if (/^Re[^a-zA-Z02-9\u00C0-\u017F]*$/.test(str)) 
+        else if (/^Re[^a-zA-Z02-9\u00C0-\u017F]*$/.test(str))
             funcs.answer("Quin" + str.substring(2) + " 🦈")
         else if (/^rE[^a-zA-Z02-9\u00C0-\u017F]*$/.test(str))
             funcs.answer("quiN" + str.substring(2) + " 🦈")
         else if (/^re[^a-z02-9\u00C0-\u017F]*$/i.test(str))
             funcs.answer("quin" + str.substring(2) + " 🦈")
     } else if (Math.random() < 1 / 10) {
-        if (/quoi[^a-z02-9\u00C0-\u017F]*$/i.test(str)) 
+        if (/quoi[^a-z02-9\u00C0-\u017F]*$/i.test(str))
             funcs.answer("feur 💇")
-        else if (/pas[^a-z02-9\u00C0-\u017F]*$/i.test(str)) 
+        else if (/pas[^a-z02-9\u00C0-\u017F]*$/i.test(str))
             funcs.answer("steque 🍉")
-        else if (/oui[^a-z02-9\u00C0-\u017F]*$/i.test(str)) 
+        else if (/oui[^a-z02-9\u00C0-\u017F]*$/i.test(str))
             funcs.answer("stiti 🐒")
     }
     return 0
